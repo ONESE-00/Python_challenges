13edc8455d18ed352f8ca2a4790a147ac7b16c62
Onese-00 <107342077+ONESE-00@users.noreply.github.com>
2022-10-13 17:04:59 +0300

    Update Parameter_counter.py

--- Parameter_counter.py
+++ Parameter_counter.py
@@ -1,4 +1,4 @@
-#returns the number of arguments passed to the function
+#returns the number of arguments passed to the function, args is not a must, one can use any par_name as long as it is preceded by a *
 def param_count(*args):
     return len(args)    # method
 print(param_count(1,2,3,4,5,6))
